dc12caef54259d5e45d6b7faad7da6c73ba694d2
Siddhant <94226898+S1D007@users.noreply.github.com>
2022-10-31 16:28:36 +0530

    Fixed Bugs

--- pages/account/signup.js
+++ pages/account/signup.js
@@ -106,6 +106,7 @@ const handleGoogleSignup = () => {
   signInWithPopup(auth, provider)
     .then((result) => {
       console.log(result)
+    route.push("/")
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
